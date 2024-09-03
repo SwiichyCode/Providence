61f5c4fe6ebf95a2f1f9cb24be929f52f8c016c5
SwiichyCode <adlpromail@gmail.com>
2024-09-03 11:59:55 +0200

    fix: modify eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,7 @@ const config = {
       }
     ],
     "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
