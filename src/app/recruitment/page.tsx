8dc6768243a7189d0769f365fb4f4ef165a28be6
SwiichyCode <adlpromail@gmail.com>
2024-09-05 12:37:43 +0200

    feat: implement cache revalidation on recruitment page

--- src/app/recruitment/page.tsx
+++ src/app/recruitment/page.tsx
@@ -2,6 +2,8 @@ import { Fragment } from "react";
 import { RecruitmentForm } from "@/core/components/recruitment/recruitment-form";
 import { db } from "@/config/server/db";
 
+export const revalidate = 60;
+
 export default async function RecruitmentPage() {
   const neededClass = await db.neededClass.findFirst();
 
